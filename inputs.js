5a042a7c714bbce8c643aa611870184eb14e6c86
Dave Pagurek <dave@davepagurek.com>
2019-10-25 11:16:12 -0700

    More sequences

--- inputs.js
+++ inputs.js
@@ -1,36 +1,42 @@
 const sequences = {
-  test: [
+  "1": [
     "data/BowTie01_tutorial.scap",
-    "data/02sketch_t_m_result_cluster.scap",
+    "data/Professional1_mixer_view2_presentation_t_m_result_cluster.scap",
+    "data/Professional3_potato_chip_view1_concept_t_m_result_cluster.scap",
+    "data/Professional1_hairdryer_view1_concept_t_m_result_cluster.scap",
+    "data/hand1_t_m_result_cluster.scap",
+  ],
+  "2": [
+    "data/BowTie01_tutorial.scap",
+    "data/Professional2_house_view2_presentation_t_m_result_cluster.scap",
+    "data/Professional3_potato_chip_view1_concept_t_m_result_cluster.scap",
     "data/03sketch_t_m_result_cluster.scap",
-    "data/04sketch_t_m_result_cluster.scap",
-    "data/05sketch_t_m_result_cluster.scap",
+    "data/mug_t_m_result_cluster.scap",
+  ],
+  "3": [
+    "data/BowTie01_tutorial.scap",
     "data/06sketch_t_m_result_cluster.scap",
-    "data/08sketch_t_m_result_cluster.scap",
-    "data/09sketch_t_m_result_cluster.scap",
     "data/11sketch_t_m_result_cluster.scap",
-    "data/Crab.scap",
+    "data/student3_house_view1_concept_t_m_result_cluster.scap",
+  ],
+  "4": [
+    "data/BowTie01_tutorial.scap",
+    "data/student1_wobble_surface_view1_presentation_t_m_result_cluster.scap",
+    "data/student3_house_view1_presentation_t_m_result_cluster.scap",
+    "data/Professional2_house_view2_concept_t_m_result_cluster.scap",
+    "data/05sketch_t_m_result_cluster.scap",
+  ],
+  "5": [
+    "data/BowTie01_tutorial.scap",
+    "data/plushie1_t_m_result_cluster.scap",
+    "data/clock_c_1_u_1_h_t_result_proximity.scap",
+  ],
+  "6": [
+    "data/BowTie01_tutorial.scap",
+    "data/hamstertoon_t_m_result_cluster.scap",
+    "data/student3_house_view1_concept_t_m_result_cluster.scap",
   ],
   short: [
     "data/BowTie01_tutorial.scap",
   ],
-  another: [
-    "data/BowTie01_tutorial.scap",
-    "data/Professional1_hairdryer_view1_concept_t_m_result_cluster.scap",
-    "data/Professional1_mixer_view2_presentation_t_m_result_cluster.scap",
-    "data/Professional2_house_view2_concept_t_m_result_cluster.scap",
-    "data/Professional2_house_view2_presentation_t_m_result_cluster.scap",
-    "data/Professional3_potato_chip_view1_concept_t_m_result_cluster.scap",
-    "data/Professional3_potato_chip_view1_presentation_t_m_result_cluster.scap",
-    "data/clock_c_1_u_1_h_t_result_proximity.scap",
-    "data/hamstertoon_t_m_result_cluster.scap",
-    "data/hand1_t_m_result_cluster.scap",
-    "data/lionhat_t_m_result_cluster.scap",
-    "data/mug_t_m_result_cluster.scap",
-    "data/plushie1_t_m_result_cluster.scap",
-    "data/student1_wobble_surface_view1_concept_t_m_result_cluster.scap",
-    "data/student1_wobble_surface_view1_presentation_t_m_result_cluster.scap",
-    "data/student3_house_view1_concept_t_m_result_cluster.scap",
-    "data/student3_house_view1_presentation_t_m_result_cluster.scap",
-  ]
 };
